27efe78a34bab274eb21b69479096e03debd9a52
Bert Chen <chenbert1211@gmail.com>
2022-08-23 22:14:49 +0000

    forgot :

--- server/api/homes.js
+++ server/api/homes.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.get('/id', async (req, res, next) =>{
+router.get('/:id', async (req, res, next) =>{
   try{
   let id = req.params.id
   const oneHomes = await Home.findByPk(id)
