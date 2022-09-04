5463f027d0e7a7830d92a1de3d48dc6438f0a0bc
Nelson Cheng-Lin <nelsonchenglin@gmail.com>
2022-09-04 14:25:29 -0400

    news page working, routed as /news for now

--- client/Routes.js
+++ client/Routes.js
@@ -10,6 +10,7 @@ import SingleHome from "./components/SingleHome";
 import Map from "./components/MapComponent";
 import HistoricChart from "./components/HistoricChart";
 import watchList from "./components/watchList";
+import News from "./components/News";
 /**
  * COMPONENT
  */
@@ -31,6 +32,7 @@ class Routes extends Component {
             <Route path="/singleHome/:id" component={SingleHome} />
             <Route path="/research" component={HistoricChart} />
             <Route path="/watchlist" component={watchList} />
+            <Route path="/news" component={News} />
           </Switch>
         ) : (
           <Switch>
@@ -41,6 +43,7 @@ class Routes extends Component {
             <Route path="/singleHome/:id" component={SingleHome} />
             <Route path="/map" component={Map} />
             <Route path="/research" component={HistoricChart} />
+            <Route path="/news" component={News} />
           </Switch>
         )}
       </div>
