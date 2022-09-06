e213129bce47665229899563f54961f0ec0ed8c5
Watson Chen <wc.watsonchen@gmail.com>
2022-09-06 18:40:49 -0400

    fixed home page and sideview syntax

--- client/components/Home.js
+++ client/components/Home.js
@@ -24,7 +24,7 @@ import {
   Grid,
 } from "@mui/material";
 import { alpha, makeStyles, styled } from "@material-ui/core/styles";
-import Footer from './footer'
+import Footer from "./footer";
 
 /**
  * COMPONENT
@@ -45,55 +45,10 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-// const marks = [
-//   { value: 0, label: "$0" },
-//   { value: 100000, label: "$100,000+" },
-//   { value: 150000, label: "$150,000+" },
-//   { value: 200000, label: "$200,000+" },
-//   { value: 250000, label: "$250,000+" },
-//   { value: 300000, label: "$300,000+" },
-//   { value: 350000, label: "$350,000+" },
-//   { value: 400000, label: "$400,000+" },
-// ];
-
 export const Home = (props) => {
   const classes = useStyles();
   const { username, history } = props;
-  const [price, setPrice] = useState(0);
-  const [beds, setBeds] = useState(0);
-  const [baths, setBaths] = useState(0);
-  const [address, setAddress] = useState("");
-  const [searchOpt, setSearchOpt] = useState(false);
-  const [minPriceOpt, setMinPriceOpt] = useState(false);
-  const [maxPriceOpt, setMaxPriceOpt] = useState(false);
-  const renderCheck = username ?'': "";
-
-  function searchClickOpen() {
-    setSearchOpt(true);
-  }
-
-  function searchClickClose() {
-    setSearchOpt(false);
-  }
-
-  function searchClick(evt) {
-    setAddress(evt.target.value);
-  }
-
-  function handlePrice(evt) {
-    let price = Number(evt.target.value);
-    setPrice(price);
-  }
-
-  function handleBeds(evt) {
-    let beds = Number(evt.target.value);
-    setBeds(beds);
-  }
-
-  function handleBaths(evt) {
-    let baths = Number(evt.target.value);
-    setBaths(baths);
-  }
+  const renderCheck = username ? "" : "";
 
   useEffect(() => {
     document.body.style.overflow = "auto";
@@ -104,132 +59,6 @@ export const Home = (props) => {
 
   return (
     <React.Fragment>
-      {/* <Container maxWidth="md">
-        <Box
-          m={2}
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          sx={{
-            bgcolor: "lightblue",
-            height: "40vh",
-            backgroundImage: `url(https://www.nawy.com/blog/wp-content/uploads/2022/07/Modern-Interior-Design.jpg)`,
-          }}
-        >
-          <Stack spacing={2}>
-            <AppBar position="static">
-              <div className={classes.searchContainer}>
-                <SearchIcon
-                  className={classes.searchIcon}
-                  onClick={() => {
-                    history.push("/map");
-                  }}
-                />
-                <TextField
-                  className={classes.searchInput}
-                  value={address}
-                  onChange={searchClick}
-                  label="Enter an address, city, or ZIP code"
-                  variant="standard"
-                />
-              </div>
-            </AppBar>
-            <Button
-              variant="contained"
-              color="primary"
-              style={{
-                height: 40,
-                width: 400,
-                background: "#72bcd4",
-              }}
-              onClick={searchClickOpen}
-            >
-              Custom your search
-            </Button>
-            <Dialog open={searchOpt} onClose={searchClickClose}>
-              <DialogContent>
-                <Box
-                  noValidate
-                  component="form"
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    zIndex: "tooltip",
-                    width: 250,
-                  }}
-                >
-                  <FormControl sx={{ m: 1, minWidth: 80 }}>
-                    <InputLabel htmlFor="price">Price</InputLabel>
-                    <NativeSelect
-                      autoFocus
-                      value={price}
-                      onChange={handlePrice}
-                      label="price"
-                      autoWidth
-                    >
-                      <option value="0">$0</option>
-                      <option value="100000">$100,000+</option>
-                      <option value="200000">$200,000+</option>
-                      <option value="300000">$300,000+</option>
-                      <option value="400000">$400,000+</option>
-                      <option value="500000">$500,000+</option>
-                    </NativeSelect>
-                  </FormControl>
-                  <FormControl sx={{ m: 1, minWidth: 80 }}>
-                    <InputLabel htmlFor="beds">Beds</InputLabel>
-                    <NativeSelect
-                      autoFocus
-                      value={beds}
-                      onChange={handleBeds}
-                      label="beds"
-                      autoWidth
-                    >
-                      <option value="0">0</option>
-                      <option value="1">1+</option>
-                      <option value="2">2+</option>
-                      <option value="3">3+</option>
-                      <option value="4">4+</option>
-                      <option value="5">5+</option>
-                    </NativeSelect>
-                  </FormControl>
-                  <FormControl sx={{ m: 1, minWidth: 80 }}>
-                    <InputLabel htmlFor="baths">Baths</InputLabel>
-                    <NativeSelect
-                      autoFocus
-                      value={baths}
-                      onChange={handleBaths}
-                      label="baths"
-                      autoWidth
-                    >
-                      <option value="0">0</option>
-                      <option value="1">1+</option>
-                      <option value="2">2+</option>
-                      <option value="3">3+</option>
-                      <option value="4">4+</option>
-                    </NativeSelect>
-                  </FormControl>
-                </Box>
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={searchClickClose}>Cancel</Button>
-                <Button onClick={searchClickClose}>Search</Button>
-              </DialogActions>
-            </Dialog>
-          </Stack>
-        </Box>
-      </Container> */}
-      <div className='imgForHome'>
-        <img src='https://www.breit.com/wp-content/uploads/sites/23/2022/03/breit-home-01.jpg'></img>
-          <Link to="/map">
-            <Button
-              variant="contained"
-              color="primary"
-              style={{ height: 40, width: 250, background: "#72bcd4" }}
-            >
-              View Map
-            </Button>
-          </Link>
-          </div>
       <Container maxWidth="md">
         <Box
           m={2}
@@ -239,10 +68,37 @@ export const Home = (props) => {
           sx={{
             bgcolor: "lightblue",
             height: "40vh",
-            marginTop: '5%',
-            margin: '10px',
-            paddingTop: '40px',
-            borderRadius: '15px' ,
+            marginTop: "5%",
+            margin: "10px",
+            paddingTop: "40px",
+            borderRadius: "15px",
+            backgroundImage: `url(https://www.bestourism.com/img/items/big/535/Philadelphia_General-view_2120.jpg)`,
+          }}
+        >
+          <Link to="/map">
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ height: 40, width: 250, background: "#72bcd4" }}
+            >
+              View Map
+            </Button>
+          </Link>
+        </Box>
+      </Container>
+      <Container maxWidth="md">
+        <Box
+          m={2}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          sx={{
+            bgcolor: "lightblue",
+            height: "40vh",
+            marginTop: "5%",
+            margin: "10px",
+            paddingTop: "40px",
+            borderRadius: "15px",
             backgroundImage: `url(https://www.nawy.com/blog/wp-content/uploads/2022/07/Modern-Interior-Design.jpg)`,
           }}
         >
@@ -267,10 +123,10 @@ export const Home = (props) => {
             sx={{
               bgcolor: "lightblue",
               height: "40vh",
-              borderRadius: '15px',
-              marginTop: '5%',
-              margin: '10px',
-              padding: '20px',
+              borderRadius: "15px",
+              marginTop: "5%",
+              margin: "10px",
+              padding: "20px",
               backgroundImage: `url(https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`,
               backgroundSize: "cover",
               backgroundPostion: "center bottom",
@@ -290,9 +146,8 @@ export const Home = (props) => {
       ) : (
         ""
       )}
-      <Footer/>
+      <Footer />
     </React.Fragment>
-    
   );
 };
 
--- client/components/SideInfoView.js
+++ client/components/SideInfoView.js
@@ -1,168 +1,171 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { PolarArea, Bar, Line } from 'react-chartjs-2';
-import { Chart, registerables } from 'chart.js';
+import { PolarArea, Bar, Line } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
 
-
 const valueCal = (color) => {
-  return (color == 'red'? 'This house is over price in the current market. It is 25% above its marketvalue'
-  : color == 'orange'? 'The price of this house for its type is over 15% market value': 
-  color == 'yellow'? 'The price is 5% above marketvalue' : color == 'green'? 'This house is currently at average cost in the current market'
-  : 'This house is currently going below the average market value. ') 
-}
-
+  return color == "red"
+    ? "This house is over price in the current market. It is 25% above its marketvalue"
+    : color == "orange"
+    ? "The price of this house for its type is over 15% market value"
+    : color == "yellow"
+    ? "The price is 5% above marketvalue"
+    : color == "green"
+    ? "This house is currently at average cost in the current market"
+    : "This house is currently going below the average market value. ";
+};
 
 class SideInfoView extends Component {
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
+  }
+
+  async componentDidMount() {}
+
+  render() {
+    console.log(this.props);
+    let { house, similar, data } = this.props;
+    let forIn;
+    let datesData;
+    let lineData;
+    if (house.beds == 1) {
+      if (data.oneBedMed.length > 0) {
+        forIn = Object.values(data.oneBedMed[0]);
+        datesData = Object.keys(data.oneBedMed[0]);
+        lineData = Object.values(data.oneBedMed[0]);
+      } else {
+        forIn = Object.values(data.oneBedMed);
+      }
+    } else if (house.beds == 2) {
+      if (data.twoBedMed.length > 0) {
+        forIn = Object.values(data.twoBedMed[0]);
+        datesData = Object.keys(data.twoBedMed[0]);
+        lineData = Object.values(data.twoBedMed[0]);
+      } else {
+        forIn = Object.values(data.twoBedMed);
+      }
+    } else if (house.beds == 3) {
+      if (data.threeBedMed.length > 0) {
+        forIn = Object.values(data.threeBedMed[0]);
+        datesData = Object.keys(data.threeBedMed[0]);
+        lineData = Object.values(data.threeBedMed[0]);
+      } else {
+        forIn = Object.values(data.threeBedMed);
+      }
+    } else if (house.beds == 4) {
+      if (data.fourBedMed.length > 0) {
+        datesData = Object.keys(data.fourBedMed[0]);
+        lineData = Object.values(data.fourBedMed[0]);
+        forIn = Object.values(data.fourBedMed[0]);
+      } else {
+        forIn = Object.values(data.fourBedMed);
+      }
+    } else if (house.beds >= 5) {
+      if (data.fiveBedMed.length > 0) {
+        datesData = Object.keys(data.fiveBedMed[0]);
+        lineData = Object.values(data.fiveBedMed[0]);
+        forIn = Object.values(data.fiveBedMed[0]);
+      } else {
+        forIn = Object.values(data.fiveBedMed);
+      }
+    } else {
+      forIn = Object.values(data.aHBedMed[0]);
     }
-  
-    async componentDidMount() {
-    }
 
-    
-    render(){
-        console.log(this.props)
-        let {house, similar, data} = this.props
-        let forIn
-        let datesData
-        let lineData 
-        if(house.beds == 1)
-        { 
-          if(data.oneBedMed.length > 0)
-          {
-          forIn = Object.values(data.oneBedMed[0])
-          datesData = Object.keys(data.oneBedMed[0])
-            lineData = Object.values(data.oneBedMed[0])
-          }else{
-            forIn = Object.values(data.oneBedMed)
-           
-          }
-        }else if(house.beds == 2)
-        {
-          if(data.twoBedMed.length > 0)
-          {
-          forIn = Object.values(data.twoBedMed[0])
-          datesData = Object.keys(data.twoBedMed[0])
-            lineData = Object.values(data.twoBedMed[0])
-          }else{
-            forIn = Object.values(data.twoBedMed)
-          }
-        }else if(house.beds == 3)
-        {
-          if(data.threeBedMed.length > 0)
-          {
-          forIn = Object.values(data.threeBedMed[0])
-          datesData = Object.keys(data.threeBedMed[0])
-            lineData = Object.values(data.threeBedMed[0])
-          }else{
-            forIn = Object.values(data.threeBedMed)
-          }
-        }else if(house.beds == 4)
-        {
-          if(data.fourBedMed.length > 0)
-          {
-            datesData = Object.keys(data.fourBedMed[0])
-            lineData = Object.values(data.fourBedMed[0])
-          forIn = Object.values(data.fourBedMed[0])}
-          else{
-            forIn = Object.values(data.fourBedMed)
-          }
-        }else if(house.beds >= 5)
-        {
-          if(data.fiveBedMed.length > 0)
-          {
-            datesData = Object.keys(data.fiveBedMed[0])
-            lineData = Object.values(data.fiveBedMed[0])
-          forIn = Object.values(data.fiveBedMed[0])
-          }else{
-            forIn = Object.values(data.fiveBedMed)
-          }
-        }else
-        {
-          forIn = Object.values(data.aHBedMed[0])
-        }
+    datesData.shift();
+    lineData.shift();
+    let collect = forIn[forIn.length - 2];
+    // console.log(collect, house.price)
 
-        datesData.shift()
-        lineData.shift()
-        let collect = forIn[forIn.length - 2]
-        // console.log(collect, house.price)
-
-        let otherHousePrice = similar.filter(homes => homes.type == house.type && homes.beds == house.beds).map(houseInfo => Number(houseInfo.priceNum))
-    return(
-        <div id="infoContainer">
-       <div className='houseContainer2' key={house.id} onClick={this.props.getSingle}>
-        <img src={house.imageURL} id={house.id}/>
-        <h3>Current listing price: {house.price}</h3>
-        <p>{house.beds} bedroooms | {house.bathrooms} bathrooms | {house.landSize}sqft</p>
-        <p>Type: {house.type} | {house.city}, {house.state} {house.zipcode}</p>
-        </div>    
+    let otherHousePrice = similar
+      .filter((homes) => homes.type == house.type && homes.beds == house.beds)
+      .map((houseInfo) => Number(houseInfo.priceNum));
+    return (
+      <div id="infoContainer">
+        <div
+          className="houseContainer2"
+          key={house.id}
+          onClick={this.props.getSingle}
+        >
+          <img src={house.imageURL} id={house.id} />
+          <h3>Current listing price: {house.price}</h3>
+          <p>
+            {house.beds} bedroooms | {house.bathrooms} bathrooms |{" "}
+            {house.landSize}sqft
+          </p>
+          <p>Type: {house.type}</p>
+          <p>
+            Location: {house.city}, {house.state} {house.zipcode}
+          </p>
+        </div>
 
         <div className="chart-graph">
-        <PolarArea data ={{
-            labels: [
-              'House',
-              'Market Value',
-            ],
-            datasets: [{
-            
-              data: [house.priceNum, collect],
-              backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(75, 192, 192)',
-                'rgb(255, 205, 86)',
-                'rgb(201, 203, 207)',
-                'rgb(54, 162, 235)'
-              ]
-            }]
-        }}/>
-        <p>{valueCal(house.color)}</p>
+          <PolarArea
+            data={{
+              labels: ["House", "Market Value"],
+              datasets: [
+                {
+                  data: [house.priceNum, collect],
+                  backgroundColor: [
+                    "rgb(255, 99, 132)",
+                    "rgb(75, 192, 192)",
+                    "rgb(255, 205, 86)",
+                    "rgb(201, 203, 207)",
+                    "rgb(54, 162, 235)",
+                  ],
+                },
+              ],
+            }}
+          />
+          <p>{valueCal(house.color)}</p>
         </div>
         <div className="chart-graph">
-<Bar data ={{
-            labels: [
-              `This house (${house.priceNum})`,
-              ...otherHousePrice
-            ],
-            datasets: [{
-              label: 'Similar houses in the neighborhood',
-              data: [house.priceNum,...otherHousePrice],
-              backgroundColor: [
-                'rgb(255, 99, 132)',
-              ]
-            }]
-        }}/>
-</div>
+          <Bar
+            data={{
+              labels: [`This house (${house.priceNum})`, ...otherHousePrice],
+              datasets: [
+                {
+                  label: "Similar houses in the neighborhood",
+                  data: [house.priceNum, ...otherHousePrice],
+                  backgroundColor: ["rgb(255, 99, 132)"],
+                },
+              ],
+            }}
+          />
+        </div>
 
-<div className="chart-graph lineGraph">
-        <Line data={{
-            labels: datesData,
-            datasets: [{
-                label: 'past market value',
-                data: lineData,
-                fill: false,
-            borderColor: ['rgb(54, 162, 235)'],
-             pointRadius: 0,
-            }],}} /></div>
-
+        <div className="chart-graph lineGraph">
+          <Line
+            data={{
+              labels: datesData,
+              datasets: [
+                {
+                  label: "past market value",
+                  data: lineData,
+                  fill: false,
+                  borderColor: ["rgb(54, 162, 235)"],
+                  pointRadius: 0,
+                },
+              ],
+            }}
+          />
+        </div>
       </div>
-      
-      )
-    }
+    );
   }
-  
-  /**
-   * CONTAINER
-   */
-  const mapStateToProps = (state) => {
-    return {
-     house: state.home.single,
-     similar: state.home.forZipcode,
-     data: state.geo.historic
-    };
+}
+
+/**
+ * CONTAINER
+ */
+const mapStateToProps = (state) => {
+  return {
+    house: state.home.single,
+    similar: state.home.forZipcode,
+    data: state.geo.historic,
   };
-  
-  export default connect(mapStateToProps)(SideInfoView);
-  
+};
+
+export default connect(mapStateToProps)(SideInfoView);
